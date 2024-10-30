d949d8d26e38161b6e1af067d38e4592e76b16c4
Jake Schaffer <jakecr8guru@gmail.com>
2024-10-30 13:39:22 -0500

    adds mdx plugins and configs while reorganizing codebase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,31 @@
+import HomePage from '@/pages/Home';
+import { getAllPosts, PostMeta } from '@/utils/getAllPosts';
+import Card from '@/components/Card';
+
 export default function Home() {
+  const posts: PostMeta[] = getAllPosts();
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <h1 className="text-4xl font-bold text-center sm:text-left">
-            Jake Unplugged
-          </h1>
-        </div>
-      </main>
+      <HomePage />
+
+      <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+        {posts.length > 0 ? (
+          posts.map((post) => (
+            <Card
+              key={post.slug}
+              title={post.title}
+              description={post.description}
+              tags={post.tags}
+              slug={post.slug}
+              image={post.img}
+            />
+          ))
+        ) : (
+          <p className="text-center text-gray-600">No blog posts found.</p>
+        )}
+      </div>
+
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <span className="text-sm text-gray-500">Made with ✨ by Jake</span>
         <span className="text-sm text-gray-500">•</span>
