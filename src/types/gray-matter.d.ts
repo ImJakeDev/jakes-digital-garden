d019004ef8e5c3570f2bf2179f7b3644ec76b6e5
Jake Schaffer <jakecr8guru@gmail.com>
2024-10-30 14:20:39 -0500

    adds eslint coments for custom types

--- src/types/gray-matter.d.ts
+++ src/types/gray-matter.d.ts
@@ -9,10 +9,12 @@ declare module 'gray-matter' {
   interface GrayMatterOptions {
     excerpt?: (file: string, options: GrayMatterOptions) => string;
     engines?: {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
       [engine: string]: (input: string) => any;
     };
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   export default function matter<T = any>(
     input: string | Buffer,
     options?: GrayMatterOptions
