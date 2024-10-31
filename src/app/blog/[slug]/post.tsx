cae977f1ba58efa942d1d048ca47bada6129a221
Jake Schaffer <jakecr8guru@gmail.com>
2024-10-31 01:14:06 -0500

    adds post page for blog slug

---
+++ src/app/blog/[slug]/post.tsx
@@ -0,0 +1,45 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+import { MDXRemote } from 'next-mdx-remote/rsc'; // Use RSC version for Server Components
+
+type BlogPostProps = {
+  params: {
+    slug: string;
+  };
+};
+
+export async function generateStaticParams() {
+  const postsDirectory = path.join(process.cwd(), 'content');
+  const filenames = fs.readdirSync(postsDirectory);
+
+  return filenames.map((filename) => ({
+    slug: filename.replace('.mdx', ''),
+  }));
+}
+
+export default async function BlogPost({ params }: BlogPostProps) {
+  console.log('Hello from BlogPost');
+
+  const { slug } = await params;
+  const postFilePath = path.join(process.cwd(), 'content', `${slug}.mdx`);
+
+  // Check if the file exists
+  if (!fs.existsSync(postFilePath)) {
+    return <div>Post not found</div>;
+  }
+
+  // Read the content
+  const postContent = fs.readFileSync(postFilePath, 'utf-8');
+  const { data, content } = matter(postContent);
+
+  // Render MDX content
+  return (
+    <div className="p-6 max-w-3xl mx-auto">
+      <h1 className="text-4xl font-bold mb-4 text-center">{data.title}</h1>
+      <div className="prose prose-lg dark:prose-dark mx-auto">
+        <MDXRemote source={content} />
+      </div>
+    </div>
+  );
+}
