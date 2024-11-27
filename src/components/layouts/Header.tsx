a30626e9dbb5cfb4abc58c82beaf6de8953eca0e
Jake Schaffer <jakecr8guru@gmail.com>
2024-11-27 17:43:42 -0600

    Adds header navigation ordered list items

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,10 +1,30 @@
 import { styled } from '@linaria/react';
+import Link from 'next/link';
 
 export default function Header() {
   return (
     <StyledHeader>
       <StyledHeaderContainer>
-        <h1>Jake Unplugged</h1>
+        <h1>
+          <Link href="/">Jake Unplugged</Link>
+        </h1>
+        {/* // Todo: Build Pages */}
+        <nav>
+          <StyledNavigationOrderedList>
+            <li>
+              <Link href="/blog">Blog</Link>
+            </li>
+            <li>
+              <Link href="/projects">Projects</Link>
+            </li>
+            <li>
+              <Link href="/about">About</Link>
+            </li>
+            <li>
+              <Link href="/contact">Contact</Link>
+            </li>
+          </StyledNavigationOrderedList>
+        </nav>
       </StyledHeaderContainer>
     </StyledHeader>
   );
@@ -12,11 +32,21 @@ export default function Header() {
 
 const StyledHeader = styled.header`
   border-bottom: 1px solid var(--color-border);
-  padding-block: var(--space-l-xl);
+  padding-block: var(--space-s-m);
 `;
 
 const StyledHeaderContainer = styled.div`
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
+
+  max-width: var(--grid-max-width);
+  padding-inline: var(--grid-gutter);
+  margin-inline: auto;
+`;
+
+const StyledNavigationOrderedList = styled.ol`
+  display: flex;
+  flex-direction: row;
+  gap: var(--space-s-m);
 `;
