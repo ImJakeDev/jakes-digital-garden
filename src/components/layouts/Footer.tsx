ad815001a265906a011337aeb07fae993d3a77dd
Jake Schaffer <jakecr8guru@gmail.com>
2024-12-23 16:39:20 -0600

    Adds useRandomEmoji

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -1,24 +1,26 @@
-import useRandomEmoji from '@/hooks/useRandomEmoji';
 import usePokémon from '@/services/hooks/usePokémon';
+import useRandomEmoji from '@/services/hooks/useRandomEmoji';
 import { styled } from '@linaria/react';
 import Image from 'next/image';
 
 export default function Footer() {
   // Todo: Enhance this feature to display a random Pokémon image or build an input to search for a specific Pokémon
-  const { data: Quagsire, isLoading } = usePokémon('quagsire');
+  const { data: Quagsire, isLoading: isLoadingQuagsire } = usePokémon('quagsire');
+  const { data: randomEmoji, isLoading: isLoadingRandomEmoji } = useRandomEmoji({});
 
-  console.log('data', Quagsire);
-  console.log('isLoading', isLoading);
+  console.log('Quagsire', Quagsire);
+  console.log('isLoadingQuagsire', isLoadingQuagsire);
 
-  const randomEmojiHexCode = useRandomEmoji();
+  console.log('randomEmoji', randomEmoji);
+  console.log('isLoadingRandomEmoji', isLoadingRandomEmoji);
 
   return (
     <StyledFooter>
       <StyledFooterContainer>
-        <span>Made with {randomEmojiHexCode} by Jake</span>
+        {!!randomEmoji && <span>Made with {randomEmoji.emoji} by Jake</span>}
         <span>•</span>
         <em>© 2024 Jake Unplugged</em>
-        {!isLoading && <Image src={Quagsire?.sprites.front_default} alt="Quagsire" width={96} height={96} />}
+        {!!Quagsire && <Image src={Quagsire.sprites.front_default} alt="Quagsire" width={96} height={96} />}
       </StyledFooterContainer>
     </StyledFooter>
   );
---
+++ src/services/hooks/useRandomEmoji.ts
@@ -0,0 +1,168 @@
+import { useQuery } from '@tanstack/react-query';
+import { fetchEmojis, Emoji } from 'emojibase';
+import EmojiMeta from 'emojibase-data/meta/groups.json';
+import filter from 'lodash.filter';
+
+const fetchRandomEmoji = () => {
+  const emojiData = fetchEmojis('en');
+
+  return emojiData;
+};
+
+type SubGroup =
+  | 'alphanum'
+  | 'animal-amphibian'
+  | 'animal-bird'
+  | 'animal-bug'
+  | 'animal-mammal'
+  | 'animal-marine'
+  | 'animal-reptile'
+  | 'arrow'
+  | 'arts-crafts'
+  | 'av-symbol'
+  | 'award-medal'
+  | 'body-parts'
+  | 'book-paper'
+  | 'cat-face'
+  | 'clothing'
+  | 'computer'
+  | 'country-flag'
+  | 'currency'
+  | 'dishware'
+  | 'drink'
+  | 'emotion'
+  | 'event'
+  | 'face-affection'
+  | 'face-concerned'
+  | 'face-costume'
+  | 'face-glasses'
+  | 'face-hand'
+  | 'face-hat'
+  | 'face-negative'
+  | 'face-neutral-skeptical'
+  | 'face-sleepy'
+  | 'face-smiling'
+  | 'face-tongue'
+  | 'face-unwell'
+  | 'family'
+  | 'flag'
+  | 'food-asian'
+  | 'food-fruit'
+  | 'food-prepared'
+  | 'food-sweet'
+  | 'food-vegetable'
+  | 'game'
+  | 'gender'
+  | 'geometric'
+  | 'hair-style'
+  | 'hand-fingers-closed'
+  | 'hand-fingers-open'
+  | 'hand-fingers-partial'
+  | 'hand-prop'
+  | 'hand-single-finger'
+  | 'hands'
+  | 'heart'
+  | 'hotel'
+  | 'household'
+  | 'keycap'
+  | 'light-video'
+  | 'lock'
+  | 'mail'
+  | 'math'
+  | 'medical'
+  | 'money'
+  | 'monkey-face'
+  | 'music'
+  | 'musical-instrument'
+  | 'office'
+  | 'other-object'
+  | 'other-symbol'
+  | 'person-activity'
+  | 'person-fantasy'
+  | 'person-gesture'
+  | 'person-resting'
+  | 'person-role'
+  | 'person-sport'
+  | 'person-symbol'
+  | 'person'
+  | 'phone'
+  | 'place-building'
+  | 'place-geographic'
+  | 'place-map'
+  | 'place-other'
+  | 'place-religious'
+  | 'plant-flower'
+  | 'plant-other'
+  | 'punctuation'
+  | 'religion'
+  | 'science'
+  | 'skin-tone'
+  | 'sky-weather'
+  | 'sound'
+  | 'sport'
+  | 'subdivision-flag'
+  | 'time'
+  | 'tool'
+  | 'transport-air'
+  | 'transport-ground'
+  | 'transport-sign'
+  | 'transport-water'
+  | 'warning'
+  | 'writing'
+  | 'zodiac';
+
+type Group = 'activities' | 'animals-nature' | 'component' | 'flags' | 'food-drink' | 'objects' | 'people-body' | 'smileys-emotion' | 'symbols' | 'travel-places';
+
+type Exclude = {
+  groups?: Group[];
+  subGroups?: SubGroup[];
+};
+
+const useRandomEmoji = ({ groups = ['flags', 'symbols', 'smileys-emotion', 'people-body', 'travel-places'], subGroups = ['arrow', 'body-parts', 'clothing', 'event', 'family', 'skin-tone', 'sport'] }: Exclude) => {
+  return useQuery({
+    queryKey: ['RandomEmoji', groups, subGroups],
+    queryFn: () => fetchRandomEmoji(),
+    staleTime: Infinity,
+    select: (emojiData): Emoji => {
+      // Todo: label: "root vegetable" Has a bug rendering the emoji
+
+      const groupExclusions = groups;
+      const subgroupExclusions = subGroups;
+
+      const switchKeysAndValues = <T extends Record<string, string>>(obj: T): Record<string, keyof T> => {
+        return Object.entries(obj).reduce(
+          (acc, [key, value]) => {
+            acc[value] = key;
+            return acc;
+          },
+          {} as Record<string, keyof T>
+        );
+      };
+
+      const emojiMetaGroups = switchKeysAndValues(EmojiMeta.groups);
+      const emojiMetaSubGroups = switchKeysAndValues(EmojiMeta.subgroups);
+
+      const excludedGroups = groupExclusions?.map((group) => emojiMetaGroups[group]);
+      const excludedSubGroups = subgroupExclusions?.map((subgroup) => emojiMetaSubGroups[subgroup]);
+
+      const filteredEmojiDataForGroups = filter(emojiData, (emoji) => {
+        return !excludedGroups.some((excludedGroup) => {
+          return !!emoji.group ? String(emoji.group) === excludedGroup : true;
+        });
+      });
+
+      const filteredEmojiDataForSubGroups = filter(filteredEmojiDataForGroups, (emoji) => {
+        return !excludedSubGroups.some((excludedSubGroup) => {
+          return !!emoji.subgroup ? String(emoji.subgroup) === excludedSubGroup : true;
+        });
+      });
+
+      const randomModifiedIndex = Math.floor(Math.random() * filteredEmojiDataForSubGroups.length);
+      const randomModifiedEmoji = filteredEmojiDataForSubGroups[randomModifiedIndex];
+
+      return randomModifiedEmoji;
+    },
+  });
+};
+
+export default useRandomEmoji;
