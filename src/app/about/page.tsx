49fb893a507cdaa674d54f73108f772b32eedcb0
Jake Schaffer <jakecr8guru@gmail.com>
2025-02-11 22:29:24 -0600

    Adds About page mdx content

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,27 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+import { MDXRemote } from 'next-mdx-remote/rsc';
 import PageContainer from '@/components/layouts/PageContainer';
+import { css } from '@linaria/core';
 
-export default function AboutPage() {
+export default async function AboutPage() {
+  const aboutFilePath = path.join(process.cwd(), 'content/pages/about.mdx');
+  const fileContent = fs.readFileSync(aboutFilePath, 'utf-8');
+  const { content } = matter(fileContent);
+
   return (
     <PageContainer>
-      <h2>About Page</h2>
+      <article className={articleStyles}>
+        <MDXRemote source={content} />
+      </article>
     </PageContainer>
   );
 }
+
+const articleStyles = css`
+  display: flex;
+  align-self: center;
+  flex-direction: column;
+  gap: var(--space-s-m);
+`;
