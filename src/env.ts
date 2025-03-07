6fcf4fd1074867423029ea8a4194ac5cc818bfc0
Jake Schaffer <jakecr8guru@gmail.com>
2025-03-07 10:50:30 -0600

    Adds geoapify api to replace Google's

--- src/env.ts
+++ src/env.ts
@@ -6,9 +6,11 @@ export const env = createEnv({
   client: {
     NEXT_PUBLIC_GOOGLE_API_KEY: z.string().min(1),
     NEXT_PUBLIC_BASE_URL: z.string().min(1),
+    NEXT_PUBLIC_GEOAPIFY_API_KEY: z.string().min(1),
   },
   runtimeEnv: {
     NEXT_PUBLIC_GOOGLE_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
     NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+    NEXT_PUBLIC_GEOAPIFY_API_KEY: process.env.NEXT_PUBLIC_GEOAPIFY_API_KEY,
   },
 });
--- src/services/hooks/useReverseGeocoding.ts
+++ src/services/hooks/useReverseGeocoding.ts
@@ -1,18 +1,21 @@
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import { env } from '../../env';
+import ms from 'milliseconds';
 
 const fetchGeolocation = async (lat: number, lng: number) => {
-  const response = axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${env.NEXT_PUBLIC_GOOGLE_API_KEY}`);
+  // const response = axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${env.NEXT_PUBLIC_GOOGLE_API_KEY}`);
+  const response = axios.get(`https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${lng}&apiKey=${env.NEXT_PUBLIC_GEOAPIFY_API_KEY}`);
+  const data = (await response).data;
 
-  return response;
+  return data;
 };
 
 const useReverseGeocoding = (lat: number, lng: number) => {
   return useQuery({
     queryKey: ['reverse-geocoding', lat, lng],
     queryFn: () => fetchGeolocation(lat, lng),
-    staleTime: Infinity,
+    staleTime: ms.hours(1),
   });
 };
 
