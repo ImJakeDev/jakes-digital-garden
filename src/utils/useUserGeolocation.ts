82b2c496a494e221c363041887b281e0bf236adb
Jake Schaffer <jakecr8guru@gmail.com>
2025-03-11 14:45:23 -0500

    Updates UI with no position data

--- src/utils/useUserGeolocation.ts
+++ src/utils/useUserGeolocation.ts
@@ -10,10 +10,6 @@ const useUserGeolocation = () => {
     navigator.geolocation.getCurrentPosition(setPosition, setError);
   }, []);
 
-  if (!position || !error) {
-    return { position: null, error: { message: 'Browser Navigator Geolocation Position Error' } };
-  }
-
   return { position, error };
 };
 
