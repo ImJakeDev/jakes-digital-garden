ccf37fdc8eb434457b9bd68048e9f7a3b8cf98ed
Jake Schaffer <jakecr8guru@gmail.com>
2025-03-11 14:36:07 -0500

    Adds and error messgae for null states

--- src/utils/useUserGeolocation.ts
+++ src/utils/useUserGeolocation.ts
@@ -1,14 +1,18 @@
+'use client';
 import { useState, useEffect } from 'react';
 
 const useUserGeolocation = () => {
   const [position, setPosition] = useState<GeolocationPosition | null>(null);
   console.log('position:', position);
-
   const [error, setError] = useState<GeolocationPositionError | null>(null);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(setPosition, setError);
   }, []);
+
+  if (!position && !error) {
+    return { position: null, error: { message: 'Browser Navigator Geolocation Position Error' } };
+  }
 
   return { position, error };
 };
